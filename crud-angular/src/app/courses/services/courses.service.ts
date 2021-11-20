b344f160752a0920fcf335edc17c6aed513372c3
Thomas Erick <thomaserick.dev@gmail.com>
2021-11-20 13:43:20 -0300

    Pipe para mostrar icone

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -7,14 +7,14 @@ import { delay, first, tap } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '/assets/1courses.json';
+  private readonly API = '/assets/courses.json';
 
   constructor(private httpClient: HttpClient) {}
 
   findAll() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      delay(5000),
+      delay(1000),
       tap((courses) => console.log(courses))
     );
   }
---
+++ crud-angular/src/app/shared/pipes/category.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'category',
+})
+export class CategoryPipe implements PipeTransform {
+  transform(value: string, ...args: unknown[]): string {
+    switch (value) {
+      case 'front-end':
+        return 'code';
+      case 'back-end':
+        return 'code';
+      default:
+        return 'code';
+    }
+  }
+}
