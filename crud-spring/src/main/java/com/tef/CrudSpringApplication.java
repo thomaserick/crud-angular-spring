821ab86799eb260f4120dcd6b8aac264dc118fc6
Thomas Erick <thomaserick.dev@gmail.com>
2021-12-01 20:26:45 -0300

    Aula14 - projeto spring-boot

---
+++ crud-spring/src/main/java/com/tef/CrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+}
